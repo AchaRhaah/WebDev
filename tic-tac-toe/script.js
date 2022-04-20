5fccadf51585fca296016ad50bbceb89c03cb474
Rha'ah <acharhaah2003@gmial.com>
2022-04-20 05:33:39 +0100

    good

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,4 +1,6 @@
 var btns = document.querySelectorAll(".btn");
+var pick = document.querySelector(".pick");
+var title = document.querySelector(".title");
 var available = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 var numArray = [];
 var computerPlayArr = [];
@@ -7,39 +9,90 @@ btns.forEach(function(btn){
         const num = parseInt(e.currentTarget.id);
         if(clickAuthentication(num, numArray)){
             btn.textContent = "X";
-            computerPlay();
+            if(checkWins(numArray)){
+                title.textContent = "You WIN!!!!";
+            }
+            setTimeout( function() { computerPlay(); }, 1000);
+            console.log(available+"me");
         }
-        alert("clicked "+num);
     });
 
 });
 function checkWins(arr){
-    for(var i = 0; i < arr.length; i++){
-        if((arr[i] == 1 && arr[i+1] == 2 && arr[i+2] == 3) ||
-        (arr[i] == 4 && arr[i+1] == 5 && arr[i+2] == 6) ||
-        (arr[i] == 7 && arr[i+1] == 8 && arr[i+2] == 9) ){
-            alert("win");
+    
+    let one = arr.indexOf(1);
+    let two = arr.indexOf(2);
+    let three = arr.indexOf(3);
+    let four = arr.indexOf(4);
+    let five = arr.indexOf(5);
+    let six = arr.indexOf(6);
+    let seven = arr.indexOf(7);
+    let eight = arr.indexOf(8);
+    let nine = arr.indexOf(9);
+    if(arr.includes(1) && arr.includes(2) && arr.includes(3)){
+        if(one > two > three || three > two > one){
+            return true;
         }
     }
+    if(arr.includes(4) && arr.includes(5) && arr.includes(6)){
+        if(four > five > six || six > five > four){
+            return true;
+        }
+    }
+    if(arr.includes(7) && arr.includes(8) && arr.includes(9)){
+        if(seven > eight > nine || nine > eight > seven){
+            return true;
+        }
+    }
+    if(arr.includes(1) && arr.includes(4) && arr.includes(7)){
+        if(one > four > seven || seven > four > one){
+            return true;
+        }
+    }
+    if(arr.includes(2) && arr.includes(5) && arr.includes(8)){
+        if(two > five > eight || eight > five > two){
+            return true;
+        }
+    }
+    if(arr.includes(3) && arr.includes(6) && arr.includes(9)){
+        if(three > six > nine || nine > six > three){
+            return true;
+        }
+    }
+    if(arr.includes(1) && arr.includes(5) && arr.includes(9)){
+        if(one > five > nine || nine > five > one){
+            return true;
+        }
+    }
+    if(arr.includes(3) && arr.includes(5) && arr.includes(7)){
+        if(three > five > seven || seven > five > three){
+            return true;
+        }
+    }
+
 }
 function computerPlay(){
     let max = Math.max.apply(null, available);
     let min = Math.min.apply(null, available);
-    var compIndexPlay = (Math.floor(Math.random() * (max - min)) + min);
-    var compIndex = compIndexPlay.toString();
-    var ID = document.getElementById(compIndex);
-    var index = available.indexOf(compIndexPlay);
+    var compIndexPlay = Math.floor(Math.random() * (max - min) + min); 
+    var ID = compIndexPlay.toString();
+    let Id = document.getElementById(ID);
     if(clickAuthentication(compIndexPlay, computerPlayArr)){
-        ID.style.background = "red";
+        Id.textContent = "O";
+        if(checkWins(computerPlayArr)){
+            title.textContent = "Computer wins";
+        };
     }
 }
+
 function clickAuthentication(value, arrToBePushed){
     if(available.includes(value)){
+
         var index = available.indexOf(value);
         available.splice(index, 1);
-        console.log(available);
+        console.log(available+"computer");
+
         arrToBePushed.push(value);
-        console.log("good");
         return true;
     }
     return false;
--- tic-tac-toe/try.js
+++ tic-tac-toe/try.js
@@ -47,3 +47,50 @@ function clickAuthentication(value, arrToBePushed){
     }
     return false;
 }
+// 
+var btns = document.querySelectorAll(".btn");
+var available = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+var numArray = [];
+var computerPlayArr = [];
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const num = parseInt(e.currentTarget.id);
+        if(clickAuthentication(num, numArray)){
+            btn.textContent = "X";
+            computerPlay();
+        }
+        alert("clicked "+num);
+    });
+
+});
+function checkWins(arr){
+    for(var i = 0; i < arr.length; i++){
+        if((arr[i] == 1 && arr[i+1] == 2 && arr[i+2] == 3) ||
+        (arr[i] == 4 && arr[i+1] == 5 && arr[i+2] == 6) ||
+        (arr[i] == 7 && arr[i+1] == 8 && arr[i+2] == 9) ){
+            alert("win");
+        }
+    }
+}
+function computerPlay(){
+    let max = Math.max.apply(null, available);
+    let min = Math.min.apply(null, available);
+    var compIndexPlay = (Math.floor(Math.random() * (max - min)) + min);
+    var compIndex = computerPlay.toString
+    var ID = document.getElementById(compIndex);
+    var index = available.indexOf(compIndexPlay);
+    if(clickAuthentication(compIndexPlay, computerPlayArr)){
+        ID.textContent = "O";
+    }
+}
+function clickAuthentication(value, arrToBePushed){
+    if(available.includes(value)){
+        var index = available.indexOf(value);
+        available.splice(index, 1);
+        console.log(available);
+        arrToBePushed.push(value);
+        console.log("good");
+        return true;
+    }
+    return false;
+}
