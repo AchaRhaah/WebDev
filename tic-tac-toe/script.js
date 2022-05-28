616e43de4a5f4ba8f56a3af4b1cc987f8eb4640e
AchaRhaah <acharhaah2003@gmail.com>
2022-05-28 19:18:25 +0100

    Revert "hey" good This reverts commit dedac383a67465d9353294b2cc99b879b16bc4d7.

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -8,6 +8,7 @@ var computerPlayArr = [];
 btns.forEach(function(btn){
     btn.addEventListener('click', function(e){
         const num = parseInt(e.currentTarget.id);
+        console.log(available, num,"me");
 
         if(clickAuthentication(num, numArray )){
             btn.textContent = "X";
@@ -15,6 +16,7 @@ btns.forEach(function(btn){
                 title.textContent = "You WIN!!!!";
             }
             setTimeout( function() { computerPlay(); }, 1000);
+            // console.log(available+"me");
         }
     });
 
@@ -31,49 +33,80 @@ function checkWins(arr){
     let eight = arr.indexOf(8);
     let nine = arr.indexOf(9);
     if(arr.includes(1) && arr.includes(2) && arr.includes(3)){
+<<<<<<< HEAD
         if(one > two > three || three > two > one || two > three > one || one > three > two || two > one > three || three > one > two){
 
+=======
+        if(one > two > three || three > two > one || three > two > one || one > two > three){
+>>>>>>> parent of dedac38... hey
             return true;
         }
     }
     if(arr.includes(4) && arr.includes(5) && arr.includes(6)){
+<<<<<<< HEAD
         if(four > five > six || six > five > four || five > six > four || four > six > five || five > four > six || six > four > five){
+=======
+        if(four > five > six || six > five > four ){
+>>>>>>> parent of dedac38... hey
             return true;
         }
     }
-
     if(arr.includes(7) && arr.includes(8) && arr.includes(9)){
+<<<<<<< HEAD
         if(seven > eight > nine || nine > eight > seven || eight > nine > seven || seven > nine > eight || eight > seven > nine || nine > seven > eight){
+=======
+        if(seven > eight > nine || nine > eight > seven){
+>>>>>>> parent of dedac38... hey
             return true;
         }
     }
     if(arr.includes(1) && arr.includes(4) && arr.includes(7)){
+<<<<<<< HEAD
         if(one > four > seven || seven > four > one || four > seven > one || one > seven > four || four > one > seven || seven > one > four){
 
+=======
+        if(one > four > seven || seven > four > one){
+>>>>>>> parent of dedac38... hey
             return true;
         }
     }
     if(arr.includes(2) && arr.includes(5) && arr.includes(8)){
+<<<<<<< HEAD
         if(five > two > eight || eight > two > five || two > eight > five || five > eight > two || two > five > eight || eight > five > two){
 
+=======
+        if(two > five > eight || eight > five > two){
+>>>>>>> parent of dedac38... hey
             return true;
         }
     }
     if(arr.includes(3) && arr.includes(6) && arr.includes(9)){
+<<<<<<< HEAD
         if(nine > six > three || three > six > nine || six > three > nine || nine > three > six || six > nine > three || three > nine > six){
 
+=======
+        if(three > six > nine || nine > six > three){
+>>>>>>> parent of dedac38... hey
             return true;
         }
     }
     if(arr.includes(1) && arr.includes(5) && arr.includes(9)){
+<<<<<<< HEAD
         if(one > five > nine || nine > five > one || five > nine > one || one > nine > five || five > one > nine || nine > one > five){
+=======
+        if(one > five > nine || nine > five > one){
+>>>>>>> parent of dedac38... hey
             return true;
         }
     }
     if(arr.includes(3) && arr.includes(5) && arr.includes(7)){
+<<<<<<< HEAD
         console.log(arr);
         if(five > seven > three || three > seven > five || seven > three > five || five > three > seven || seven > five > three || three > five > seven){
 
+=======
+        if(three > five > seven || seven > five > three){
+>>>>>>> parent of dedac38... hey
             return true;
         }
     }
@@ -84,6 +117,7 @@ function computerPlay(){
     let min = Math.min.apply(null, available);
     var compIndexPlay = Math.floor(Math.random() * (max - min) + min);
     if(available.includes(compIndexPlay)){
+        console.log(available, compIndexPlay,"computer");
         var ID = compIndexPlay.toString();
         let Id = document.getElementById(ID);
         if(clickAuthentication(compIndexPlay, computerPlayArr)){
@@ -92,10 +126,10 @@ function computerPlay(){
             title.textContent = "Computer wins";
         };
         }
-    } 
+    }
     else{
         computerPlay()
-    }
+    } 
     
 }
 
@@ -105,6 +139,7 @@ function clickAuthentication(value, arrToBePushed){
 
         var index = available.indexOf(value);
         available.splice(index, 1);
+
         arrToBePushed.push(value);
         return true;
     }
