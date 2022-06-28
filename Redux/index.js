eff49cfba073cbcb4fb62b67bc80ba11c4a13059
AchaRhaah <acharhaah2003@gmail.com>
2022-06-28 18:46:11 +0100

    installed packages and dependencies

--- Redux/index.js
+++ Redux/index.js
@@ -6,3 +6,22 @@ function buyCake() {
     info: "First redux action",
   };
 }
+
+// reducers
+
+// (previousState, action) => newstate
+
+const initialState = {
+  numOfCakes: 10,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
