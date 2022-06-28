60cc1074e48825169afbb2163b754e17e6673560
AchaRhaah <acharhaah2003@gmail.com>
2022-06-28 18:02:24 +0100

    action creator

--- Redux/index.js
+++ Redux/index.js
@@ -1,8 +1,8 @@
-const BUY_CAKE = 'BUY_CAKE'
-
-{
-    type: BUY_CAKE,
-    info:  'First redux action'
-}
-
+const BUY_CAKE = "BUY_CAKE";
 // action creator
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}
