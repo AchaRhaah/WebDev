6562fa63d53ca97922735f2e1e8b91a3e9783de6
AchaRhaah <acharhaah2003@gmail.com>
2022-08-05 21:41:46 +0100

    redux basics

--- Redux/index.js
+++ Redux/index.js
@@ -1,37 +1,76 @@
 const redux = require("redux");
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+
+// // action are javaScript object with a type property
 const BUY_CAKE = "BUY_CAKE";
-// action creator
+const BUY_ICECREAM = "BUY_ICECREAM";
+
+// {
+//   type: BUY_CAKE,
+//   info: 'First redux action'
+// }
+// action creator is a function that returns an action
+
 function buyCake() {
   return {
     type: BUY_CAKE,
     info: "First redux action",
   };
 }
-// reducers
-// (previousState, action) => newstate
-const initialState = {
+function buyIcecream() {
+  return {
+    type: BUY_ICECREAM,
+  };
+}
+
+// A reducer is a function that accepts
+// state and action as arguements, and returns the next state of the application
+// eg (previousState, action) => newState
+
+const initialStateOfCake = {
   numOfCakes: 10,
 };
-const reducer = (state = initialState, action) => {
+const initialStateOfIceCream = {
+  numberOfIceCreame: 20,
+};
+
+const cakeReducer = (state = initialStateOfCake, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       };
+
     default:
       return state;
   }
 };
-const store = redux.createStore();
-console.log("initial state", store.getState());
-const unsubscribe = store.subscribe(() =>
-  console.log("updated state", store.getState())
-);
-store.dispatch(buyCake);
-store.dispatch(buyCake);
-store.dispatch(buyCake);
-store.dispatch(buyCake);
-store.dispatch(buyCake);
-store.dispatch(buyCake);
-unsubscribe();
+const iceCreameReducer = (state = initialStateOfIceCream, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numberOfIceCreame: state.numberOfIceCreame - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+
+const rootReducers = combineReducers({
+  cake: cakeReducer,
+  iceCreame: iceCreameReducer
+})
+const store = createStore(rootReducers);
+console.log("Initial state", store.getState())
+const unsubscribe = store.subscribe(() => console.log('*updated state', store.getState()))
+// store.subscribe(() => console.log("updated state", store.getState()));
+// store.dispatch(buyCake());
+// store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIcecream())
+unsubscribe()
+// console.log("***initial state", store.getState());
