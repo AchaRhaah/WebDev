268f509c773805e8061e96229631c8cae1f4e628
AchaRhaah <acharhaah2003@gmail.com>
2022-08-25 19:30:05 +0100

    added devtools extension

--- Redux/cakeShopApp/src/redux/store.js
+++ Redux/cakeShopApp/src/redux/store.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducers from './rootReducer';
 import logger from 'redux-logger'
 import cakeReducer from './cake/cakeReducer'
 
-const store = createStore(rootReducers, applyMiddleware(logger));
+const store = createStore(rootReducers, composeWithDevTools(applyMiddleware(logger)));
 
 export default store
