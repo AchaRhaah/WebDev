b5ceb84a4eb721dce22a35ec92d8b77f074efe39
AchaRhaah <acharhaah2003@gmail.com>
2022-08-25 18:31:16 +0100

    added redux logger which logs react states

--- Redux/cakeShopApp/src/redux/store.js
+++ Redux/cakeShopApp/src/redux/store.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducers from './rootReducer';
+import logger from 'redux-logger'
 import cakeReducer from './cake/cakeReducer'
-const store = createStore(rootReducers);
+
+const store = createStore(rootReducers, applyMiddleware(logger));
 
 export default store
