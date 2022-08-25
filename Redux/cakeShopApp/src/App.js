37c446801508ebb2729ef7028068296e6a263e3e
AchaRhaah <acharhaah2003@gmail.com>
2022-08-25 20:24:43 +0100

    added redux devtools to the react app

--- Redux/cakeShopApp/src/App.js
+++ Redux/cakeShopApp/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css"
 import { Provider } from 'react-redux'
 import store from "./redux/store";
-// import CakeContainer from "./components/CakeContainer";
+import CakeContainer from "./components/CakeContainer";
 import HooksCakeConainer from "./components/HooksCakeConainer";
 import IcecreamContainer from "./components/IcecreamContainer";
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeConainer />
         <IcecreamContainer />
       </div>
     </Provider>
