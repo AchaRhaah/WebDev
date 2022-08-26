ba47a176755c662ecabe8b5f6b4597942227fb91
AchaRhaah <acharhaah2003@gmail.com>
2022-08-26 02:26:38 +0100

    used payload to buy more than one item

--- Redux/cakeShopApp/src/App.js
+++ Redux/cakeShopApp/src/App.js
@@ -3,14 +3,14 @@ import "./App.css"
 import { Provider } from 'react-redux'
 import store from "./redux/store";
 import CakeContainer from "./components/CakeContainer";
-import HooksCakeConainer from "./components/HooksCakeConainer";
-import IcecreamContainer from "./components/IcecreamContainer";
+// import HooksCakeConainer from "./components/HooksCakeConainer";
+// import IcecreamContainer from "./components/IcecreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <HooksCakeConainer />
-        <IcecreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
---
+++ Redux/cakeShopApp/src/components/NewCakeContainer.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { buyCake } from "../redux/index";
+import { connect } from "react-redux";
+
+function NewCakeContainer(props) {
+  const [number, setNumber] = useState(1);
+  return (
+    <div>
+      <h2>Number of cakes - {props.numOfCakes} </h2>
+      <input
+        type="text"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <button onClick={() => props.buyCake(number)}>Buy {number} cake</button>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.cake.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: (number) => dispatch(buyCake(number)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewCakeContainer);
