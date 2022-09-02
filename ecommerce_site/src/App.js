9f96090866361e7d5595c923aa20afaafcfbaed8
AchaRhaah <acharhaah2003@gmail.com>
2022-09-02 12:26:06 +0100

    done with navbar

---
+++ ecommerce_site/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
