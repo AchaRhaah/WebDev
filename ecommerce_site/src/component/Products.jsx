25840344ff7b43e3090f0e4f07029d55196c4172
AchaRhaah <acharhaah2003@gmail.com>
2022-09-04 23:57:49 +0100

    products loaded onto the website successfully

--- ecommerce_site/src/component/Products.jsx
+++ ecommerce_site/src/component/Products.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 export default function Products() {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(data);
@@ -14,24 +13,57 @@ export default function Products() {
         setData(await response.clone().json());
         setFilter(await response.json());
         setLoading(false);
-        console.log("jhsldkgs" + filter);
       }
       return () => {
         componentMounted = false;
       };
     };
     getProduct();
-  });
+  }, []);
 
   const Loading = () => {
     return <>Loading...</>;
   };
   const ShowProduct = () => {
     return (
-      <div className="buttons">
-        <buttons className="btn btn-outline-dark">All</buttons>
-        <buttons className="btn btn-outline-dark">Men's Clothing</buttons>
-      </div>
+      <>
+        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+          <buttons className="btn btn-outline-dark me-2">All</buttons>
+          <buttons className="btn btn-outline-dark me-2">
+            Men's Clothing
+          </buttons>
+          <buttons className="btn btn-outline-dark me-2">
+            Women's Clothing
+          </buttons>
+          <buttons className="btn btn-outline-dark me-2">Jewelery</buttons>
+          <buttons className="btn btn-outline-dark me-2">Electronic</buttons>
+        </div>
+        {filter.map((product) => {
+          return (
+            <>
+              <div className="col-md-3 mb-4">
+                <div className="card h-100 text-center p-4" key={product.id}>
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    className="card-img-top"
+                    height="270px"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title mb-0">
+                      {product.title.substring(0, 12)}...
+                    </h5>
+                    <p className="card-text lead fw-bold">${product.price}</p>
+                    <a href="" className="btn btn-outline-dark">
+                      Buy Now
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </>
     );
   };
   return (
