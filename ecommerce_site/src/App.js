c69670a191a157d7511fc0c6dc64d28ecb761673
AchaRhaah <acharhaah2003@gmail.com>
2022-09-06 22:34:09 +0100

    .

--- ecommerce_site/src/App.js
+++ ecommerce_site/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Navbar from './component/Navbar';
 import Home from './component/Home';
+import {Route, Routes} from 'react-router-dom'
+import Products from './component/Products';
+import Product from './component/Product';
+
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/products" element={<Products/>} />
+        <Route exact path="/products/:id" element={<Product/>} />
+      </Routes>
     </>
   );
 }
