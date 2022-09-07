33b34d7064962f435244594c2ced8cbdac1661bb
AchaRhaah <acharhaah2003@gmail.com>
2022-09-07 22:30:10 +0100

    done with UI layout

--- ecommerce_site/src/component/Navbar.js
+++ ecommerce_site/src/component/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
-
+import {NavLink} from 'react-router-dom'
 function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-light  bg-light py-3 shadow-sm">
         <div className="container">
-          <a className="navbar-brand fw-bold fs-4" href="#">
+          <NavLink className="navbar-brand fw-bold fs-4" to="/">
             LA COLLECTION
-          </a>
+          </NavLink>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,36 +22,36 @@ function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <NavLink className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Product
-                </a>
+                <NavLink className="nav-link" to="/products">
+                  Products
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <NavLink className="nav-link" to="/about">
                   about
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <NavLink className="nav-link" to="/contact">
                   Contact
-                </a>
+                </NavLink>
               </li>
             </ul>
             <div className="buttons ms-2">
-              <a href="" className="btn btn-outline-dark">
+              <NavLink to="/login" className="btn btn-outline-dark">
                 <i className="fa fa-sign-in me-1"></i>Login
-              </a>
-              <a href="" className="btn btn-outline-dark ms-2">
+              </NavLink>
+              <NavLink to="/register" className="btn btn-outline-dark ms-2">
                 <i className="fa fa-user-plus me-1"></i>Register
-              </a>
-              <a href="" className="btn btn-outline-dark ms-2">
+              </NavLink>
+              <NavLink to="/cart" className="btn btn-outline-dark ms-2">
                 <i className="fa fa-shopping-cart me-1"></i>Cart (0)
-              </a>
+              </NavLink>
             </div>
           </div>
         </div>
--- ecommerce_site/src/component/Products.js
+++ ecommerce_site/src/component/Products.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import {NavLink} from 'react-router-dom'
 import Skeleton from "react-loading-skeleton";
 export default function Products() {
   const [data, setData] = useState([]);
@@ -95,9 +96,9 @@ console.log(data)
                       {product.title.substring(0, 12)}...
                     </h5>
                     <p className="card-text lead fw-bold">${product.price}</p>
-                    <a href="" className="btn btn-outline-dark">
+                    <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">
                       Buy Now
-                    </a>
+                    </NavLink>
                   </div>
                 </div>
               </div>
