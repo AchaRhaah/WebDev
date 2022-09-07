a6033f77dff9e7fb5fb102f97ce1ed38483b1713
AchaRhaah <acharhaah2003@gmail.com>
2022-09-07 22:37:45 +0100

    action creators

--- ecommerce_site/src/redux/action/index.js
+++ ecommerce_site/src/redux/action/index.js
@@ -1,1 +1,13 @@
-/
+export const addCart = (product) => {
+  return {
+    type: "AddCART",
+    payload: product,
+  };
+};
+
+export const delCart = (product) => {
+  return {
+    type: "DELITEM",
+    payload: product,
+  };
+};
