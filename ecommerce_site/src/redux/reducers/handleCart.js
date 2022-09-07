8db82d6db0fdb64be476b5d472e35c09bb27f86d
AchaRhaah <acharhaah2003@gmail.com>
2022-09-07 22:42:14 +0100

    working on the reducer function

---
+++ ecommerce_site/src/redux/reducers/handleCart.js
@@ -0,0 +1,17 @@
+const cart = [];
+const handleCart = (state = cart, action) => {
+  const product = action.payload;
+  switch (action.type) {
+      case ADDITEM:
+        //   checking if the product already exisit
+          const exist = state.find((x) => x.id === product.id);
+          if (exist) {
+              return state.map((x)=>x.id === product.id? )
+          }
+      break;
+    default:
+      break;
+  }
+};
+export default handleCart;
+// 38:47
