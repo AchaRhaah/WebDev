d2b267ef003fec4cfb976eac6bfb8fb076d5eeda
AchaRhaah <acharhaah2003@gmail.com>
2022-09-10 21:11:23 +0100

    trying to get the add and subtraction button to work

--- ecommerce_site/src/component/Cart.js
+++ ecommerce_site/src/component/Cart.js
@@ -1,17 +1,46 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import handleCart from "../redux/reducers/handleCart";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+// import handleCart from "../redux/reducers/handleCart";
 
 export default function Cart() {
   const state = useSelector((state) => state.handleCart);
-//   console.log(state);
+  };
 
   return (
     <>
           {state.map((product) => {
-              return(
-              <h1>{ product.price}</h1>)
+          const [qty, setQty] = useState(product.qty)
+        return (
+          <div className="d-flex mx-auto">
+            <div className="col-md-5">
+              <img
+                src={product.image}
+                alt={product.title}
+                height="300px"
+                width="340px"
+              />
+            </div>
+            <div className="col-md-6">
+              <h6 className="display-5 fw-bold">{product.title}</h6>
+              <h3 className="display-6 ">
+                {product.qty} &times; $ {product.price} = {product.price}
+              </h3>
+              {/* <button
+                className="btn btn-outline-dark me-4"
+              >
+                <i className="fa fa-minus"></i>
+              </button> */}
+              {/* <button
+                className="btn btn-outline-dark"
+              >
+                <i className="fa fa-plus"></i>
+              </button> */}
+            </div>
+          </div>
+        );
       })}
+      <br />
+      <br />
     </>
   );
 }
