89e25c23f31577cbb2c8d1ef4c326463eb2e9502
AchaRhaah <acharhaah2003@gmail.com>
2022-09-10 21:28:11 +0100

    got the numbers to change although its not correct yet

--- ecommerce_site/src/component/Cart.js
+++ ecommerce_site/src/component/Cart.js
@@ -4,12 +4,16 @@ import { useSelector } from "react-redux";
 
 export default function Cart() {
   const state = useSelector((state) => state.handleCart);
+  const [prodQty, setProdQty] = useState(3);
+
+  const handleButton = (prod) => {
+    setProdQty((prevQty) => prevQty + 1);
+    console.log(prodQty);
   };
 
   return (
     <>
-          {state.map((product) => {
-          const [qty, setQty] = useState(product.qty)
+      {state.map((product) => {
         return (
           <div className="d-flex mx-auto">
             <div className="col-md-5">
@@ -23,14 +27,16 @@ export default function Cart() {
             <div className="col-md-6">
               <h6 className="display-5 fw-bold">{product.title}</h6>
               <h3 className="display-6 ">
-                {product.qty} &times; $ {product.price} = {product.price}
+                {prodQty} &times; $ {product.price} = {product.price}
               </h3>
-              {/* <button
+              <button
+                onClick={handleButton}
                 className="btn btn-outline-dark me-4"
               >
                 <i className="fa fa-minus"></i>
-              </button> */}
+              </button>
               {/* <button
+                onclick={() => handleButton(product)}
                 className="btn btn-outline-dark"
               >
                 <i className="fa fa-plus"></i>
