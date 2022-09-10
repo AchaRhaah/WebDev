93dab4902212bc7459dcc3b6da7a344c07393518
AchaRhaah <acharhaah2003@gmail.com>
2022-09-10 15:19:12 +0100

    can't seem to get the cart constituents to display

--- ecommerce_site/src/component/Cart.js
+++ ecommerce_site/src/component/Cart.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import handleCart from "../redux/reducers/handleCart";
 
 export default function Cart() {
+  const state = useSelector((state) => state.handleCart);
+//   console.log(state);
+
   return (
-      <div>
-          
-    </div>
-  )
+    <>
+          {state.map((product) => {
+              return(
+              <h1>{ product.price}</h1>)
+      })}
+    </>
+  );
 }
