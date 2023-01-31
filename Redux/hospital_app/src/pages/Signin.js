c94615da24d08f5594bba9ceb0847ce9e9243f74
AchaRhaah <acharhaah2003@gmail.com>
2023-01-31 05:58:23 +0100

    collected user info

--- Redux/hospital_app/src/pages/Signin.js
+++ Redux/hospital_app/src/pages/Signin.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 function Signin() {
+  const [signinInfo, setSigninInfo] = useState({
+    email: "",
+    password: ""
+  })
+  const handleChange = (e) => {
+    setSigninInfo(prevState => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  }
+  const HandleSubmit = (e) => {
+    e.preventDefault()
+    console.log(signinInfo)
+
+  }
   return (
     <div className="w-full h-screen bg-[#f8f9fa]  flex justify-center items-center ">
       <div className="w-[28%] h-[80%] rounded-l-[3em] bg-[#3C5FC3]"></div>
       <div className="w-[32%] h-[80%] px-10 bg-[#EFEEFE] overflow-hidden rounded-r-[3em]">
         <h2 className="text-3xl text-[#141835] mt-24 font-bold">SIGNIN</h2>
-        <form action="" className="w-[100%] h-[90%] my-[3em] flex-col items-center">
-          <div className="flex gap-[1em]">
-          </div>
+        <form
+        onSubmit={HandleSubmit}
+          action=""
+          className="w-[100%] h-[90%] my-[3em] flex-col items-center"
+        >
+          <div className="flex gap-[1em]"></div>
           <div className="mb-5">
             <label htmlFor="" className="block text-[13px] font-thin">
               EMAIL
             </label>
             <input
+              id="email"
+              onChange={handleChange}
               type="text"
               className="w-[85%] h-[2.5em] rounded-[0.75em] px-2 focus: outline-[#5D67A3]"
             />
           </div>
-          
           <div className="mb-5">
             <label htmlFor="" className="block text-[13px] px-2 font-thin">
               PASSWORD
             </label>
             <input
+              id="password"
+              onChange={handleChange}
               type="password"
               className="w-[85%] h-[2.5em] rounded-[0.75em] px-2 focus: outline-[#5D67A3]"
             />
--- Redux/hospital_app/src/pages/Signup.js
+++ Redux/hospital_app/src/pages/Signup.js
@@ -1,18 +1,41 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
 function Signup() {
+
+  const [userInfo, setUserInfo] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: '', 
+    phoneNumber: 0,
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setUserInfo(prevState => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userInfo)
+  }
+  
   return (
     <div className="w-full h-screen bg-[#f8f9fa]  flex justify-center items-center ">
       <div className="w-[28%] h-[80%] rounded-l-[3em] bg-[#3C5FC3]"></div>
       <div className="w-[32%] h-[80%] px-10 bg-[#EFEEFE] overflow-hidden rounded-r-[3em]">
         <h2 className="text-3xl text-[#141835] mt-14 font-bold">Sign Up</h2>
-        <form action="" className="w-[100%] h-[90%] my-5">
+        <form action="" onSubmit={handleSubmit} className="w-[100%] h-[90%] my-5">
           <div className="flex gap-[1em]">
             <div className="mb-5">
               <label htmlFor="" className="block text-[13px] font-thin">
                 FIRST NAME
               </label>
               <input
+                onChange={handleChange}
+                id="firstName"
                 type="text"
                 className="w-[4] h-[2.5em] rounded-[0.75em] px-2 focus: outline-[#5D67A3]"
               />
@@ -22,6 +45,8 @@ function Signup() {
                 LAST NAME
               </label>
               <input
+                onChange={handleChange}
+                id="lastName"
                 type="text"
                 className="w-[4] h-[2.5em] rounded-[0.75em] px-2 focus: outline-[#5D67A3]"
               />
@@ -32,6 +57,8 @@ function Signup() {
               EMAIL
             </label>
             <input
+              onChange={handleChange}
+              id="email"
               type="text"
               className="w-[85%] h-[2.5em] rounded-[0.75em] px-2 focus: outline-[#5D67A3]"
             />
@@ -41,6 +68,8 @@ function Signup() {
               ADDRESS
             </label>
             <input
+              onChange={handleChange}
+              id="address"
               type="text"
               className="w-[85%] h-[2.5em] rounded-[0.75em] px-2 focus: outline-[#5D67A3]"
             />
@@ -50,6 +79,8 @@ function Signup() {
               PHONE NUMBER
             </label>
             <input
+              onChange={handleChange}
+              id="phoneNumber"
               type="text"
               className="w-[85%] h-[2.5em] rounded-[0.75em] px-2 focus: outline-[#5D67A3]"
             />
@@ -59,6 +90,8 @@ function Signup() {
               PASSWORD
             </label>
             <input
+              onChange={handleChange}
+              id="passwor"
               type="password"
               className="w-[85%] h-[2.5em] rounded-[0.75em] px-2 focus: outline-[#5D67A3]"
             />
@@ -68,11 +101,14 @@ function Signup() {
               CONFIRM PASSWORD
             </label>
             <input
+              onChange={handleChange}
               type="password"
               className="w-[85%] h-[2.5em] rounded-[0.75em] px-2 focus: outline-[#5D67A3]"
             />
           </div>
-          <NavLink to='/signin' className='block underline mb-5'>Aready have an account</NavLink>
+          <Link to="/signin" className="block underline mb-5">
+            Aready have an account
+          </Link>
           <button className="w-[10em] h-[3em]  rounded-[0.75em] bg-[#6646FD] text-white content-center hover:bg-[#8771f3] duration-[1000] ease-in">
             SUBMIT
           </button>
